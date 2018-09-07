129ee5576322c279dfd860959fdbd0a48dc6e5a3
amenraprod <amenraprod@gmail.com>
2018-09-06 23:39:00 -0400

    added 2nd KV pair

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 
 const question = new Map();
 question.set('question',
-'what is the newest name for the latest version of Javascript?')
-question.set(1, 'ES5')
+'what is the newest name for the latest version of Javascript?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');

538639bbbf2682335c4e07e1695a0fc76ddf482f
amenraprod <amenraprod@gmail.com>
2018-09-06 23:43:50 -0400

    added closers

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ question.set(2, 'ES6');
 question.set(3, 'ES6');
 question.set(4, 'ES7');
 question.set('correct', 3);
-question.set(true, 'correct answer :D')
-question.set(false, 'wrong answer :('))
+question.set(true, 'correct answer :D');
+question.set(false, 'wrong answer :(');

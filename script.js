86cdf5fd0c3a72502f0981e127254e6b6779d533
amenraprod <amenraprod@gmail.com>
2018-09-09 19:43:58 -0400

    initializing app

---
+++ script.js
@@ -0,0 +1,31 @@
+//////Maps.js
+
+const question = new Map();
+question.set('question',
+'what is the newest name for the latest version of Javascript?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES6');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'correct answer :D');
+question.set(false, 'wrong answer :(');
+
+console.log(question.get('question'));
+
+question.delete(4);
+
+//if(question.has(4)){
+//  question.delete(4))
+console.log('the value is here');
+//}
+//question.clear();
+//question.forEach((value, key) => console.log(`This is ${key} and it's
+//
+
+for (let [key, value] of question.entries((
+){
+    if(typeof(key === 'number'))
+    console.log(`Answer {$key}:{$value}`);
+
+const ans = parseInt(prompt('Write the correct answer'));
